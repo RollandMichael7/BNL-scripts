44cf34222fba22ba28491b97642d519c8ec79e8a
mrolland <mrolland@debian.bnl.gov>
2019-02-01 14:38:00 -0500

    add areaDetector configuration and compilation

--- AD/buildAD.py
+++ AD/buildAD.py
@@ -1,4 +1,14 @@
+# This script downloads and compiles EPICS base, EPICS modules, and AreaDetector as a synApps package
+#
+# Author: Michael Rolland
+# Copyright (c): Brookhaven National Laboratory
+# Created: February 1, 2019
+
+
+
 import subprocess
+import fileinput
+import sys
 import os
 
 # for some reason EPICS modules has no central repo, so we have to hardcode in a list of modules
@@ -14,6 +24,14 @@ ad_dependents = [ "quadEM", "dxp", "dxpSITORO" ]
 # subprocess.run takes a list of arguments
 def run(command):
 	subprocess.run(command.split())
+
+# ensure dependencies
+# run("sudo apt install libreadline-dev")
+# run("sudo apt install libx11-dev")
+# run("sudo apt install libboost-dev")
+# run("sudo apt install libboost-test-dev")
+# run("sudo apt install re2c")
+# run("sudo apt install libxext-dev")
 
 # download EPICS base
 run("git clone -b 7.0 https://git.launchpad.net/epics-base epics-base-7-0-1-1")
@@ -43,7 +61,6 @@ for folder in os.listdir("support/areaDetector"):
 		run("git -C support/areaDetector/"+folder+" checkout master")
 
 # configuration
-# throw out the old RELEASE file
 run("rm support/configure/RELEASE")
 with open("RELEASE", "w") as release:
 	release.write("SUPPORT="+os.getcwd()+"/support\n")
@@ -64,6 +81,48 @@ with open("RELEASE", "w") as release:
 run("mv RELEASE support/configure/")
 run("make -C support/ release")
 
-# compile
+# compile EPICS stuff
 run("make -C epics-base-7-0-1-1 -sj")
 run("make -C support -sj")
+
+
+################ areaDetector ################
+
+# copy from examples
+wd = os.getcwd()
+os.chdir(wd + "/support/areaDetector/configure")
+run("bash copyFromExample")
+os.chdir(wd)
+run("rm support/areaDetector/configure/EXAMPLE*")
+
+# modify configuration files
+config_files = ["RELEASE_LIBS.local", "RELEASE_PRODS.local", "RELEASE_SUPPORT.local"]
+for config in config_files:
+	for line in fileinput.input("support/areaDetector/configure/"+config, inplace=True):
+		if line.strip().startswith("#"):
+			print(line, end="")
+			continue
+		found=False
+		macro = line.split("=")[0]
+		for module in epics_modules:
+			if module.upper() == macro:
+				print(macro+"=$(SUPPORT)/"+module)
+				found=True
+				break
+		if not found:
+			if "AREA_DETECTOR=" in line:
+				print("AREA_DETECTOR=$(SUPPORT)/areaDetector")
+			elif "EPICS_BASE=" in line:
+				print("EPICS_BASE="+wd+"/epics-base-7-0-1-1")
+			elif "SUPPORT=" in line:
+				print("SUPPORT="+wd+"/support")
+			elif "DEVIOCSTATS=" in line:
+				print("DEVIOCSTATS=$(SUPPORT)/iocStats")
+			elif "SNCSEQ=" in line:
+				print("SNCSEQ=$(SUPPORT)/seq-2-2-5")
+			else:
+				print(line, end="")
+
+# compile
+run("make -C support/areaDetector/ADSupport -sj")
+run("make -C support/areaDetector/ADCore -sj")
